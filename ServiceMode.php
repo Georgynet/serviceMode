10493a04ce9477999f5adbc48b6c2c0257229a25
Georg Spack <georgynet@gmail.com>
2016-10-09 21:30:06 +0300

    service mode

--- ServiceMode.php
+++ ServiceMode.php
@@ -8,6 +8,7 @@
 
 namespace georgynet\serviceMode;
 
+use Yii;
 use yii\base\Component;
 
 class ServiceMode extends Component
@@ -17,7 +18,8 @@ class ServiceMode extends Component
     public function init()
     {
         if ($this->serviceMode) {
-            \Yii::$app->catchAll = ['service/index'];
+            Yii::$app->controllerMap['service'] = 'georgynet\serviceMode\ServiceController';
+            Yii::$app->catchAll = ['service/index'];
         }
     }
 }
